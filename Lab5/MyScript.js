c16aaae693f120676efb6181df9079ab8a01b116
goodwell42 <goodwell1998@gmail.com>
2018-10-09 14:25:17 +0800

    final_commit

--- Lab5/MyScript.js
+++ Lab5/MyScript.js
@@ -1,15 +1,15 @@
 function MyRandom() {
-	var elem = document.getElementById("p");
-	elem.style.color = "red";
-	elem.style.fontSize= "35px" ;
-	var num = Math.floor(Math.random()*10) % 3;
-	var ans;
-	if(num === 0) {
-		ans = "yes";
-	} else if(num === 1) {
-		ans = "No";
-	} else {
-		ans = "Maybe";
-	}
-	elem.innerHTML = ans; 
+	var elem = document.getElementsByTagName("p");
+	for (var i = elem.length - 1; i >= 0; i--) {
+		var num = Math.floor(Math.random()*10) % 3;
+		var ans;
+		if(num === 0) {
+			ans = "Yes!";
+		} else if(num === 1) {
+			ans = "No!";
+		} else {
+			ans = "Maybe!";
+		}
+		elem[i].innerHTML = ans; 
+	}	
 }
